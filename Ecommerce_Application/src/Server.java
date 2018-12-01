c21438e2b322939b7dc35358ba03c431969d0796
Mitchell, Jordan <jordan-mitchell-1@uiowa.edu>
2018-12-01 15:12:55 -0600

    Implemented Server characteristics: -add Socket connections -create input/output streams -process information sent from client -close connections

--- Ecommerce_Application/src/Server.java
+++ Ecommerce_Application/src/Server.java
@@ -1,4 +1,9 @@
+import com.sun.org.apache.bcel.internal.classfile.Unknown;
+
+import java.io.EOFException;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
 
@@ -17,13 +22,98 @@ public class Server {
     private Socket connection; //connection to client
 
     /**
+     * ObjectOutputStream object, allows server to send packets to client
+     */
+    private ObjectOutputStream output; //output stream to client
+
+    /**
+     * ObjectInputStream object, allows server to receive packets from client
+     */
+    private ObjectInputStream input; // input stream to client
+
+    /**
      * Server class constructor, creates the initializes the ServerSocket object
      */
     public Server(){
         try{
             server = new ServerSocket(123, 100);
+            try{
+                waitForConnection();
+                getStreams();
+                processConnection();
+                closeConnections();
+            }
+            catch (EOFException eofException){
+                System.out.println("Server terminated connection");
+            }
         }
         catch (IOException ioException){
+            ioException.printStackTrace();
+        }
+    }
+
+    /**
+     * Connects to client and initializes Socket object
+     * @throws IOException if connection failed, throws this exception
+     */
+    private void waitForConnection() throws IOException{
+        connection = server.accept();
+    }
+
+    /**
+     * Receives input and output streams from client to allow packet transfers.
+     * @throws IOException if connection failed, throws this exception
+     */
+    private void getStreams() throws IOException{
+        output = new ObjectOutputStream(connection.getOutputStream());
+        output.flush(); //send message to client of server information
+
+        input = new ObjectInputStream(connection.getInputStream());
+
+        System.out.println("Got I/O Streams");
+    }
+
+    /**
+     * Sends and receives packets of information from client
+     * @throws IOException if connection failed, throws this exception
+     */
+    private void processConnection() throws IOException{
+        String message = "Connection Successful";
+        sendData(message);
+        do {
+            try{
+                message = (String) input.readObject();
+                System.out.println(message);
+            } catch (ClassNotFoundException classNotFoundException){
+                System.out.println("Unknown object type received");
+            }
+        } while ( !message.equals("Client>>TERMINATE"));
+    }
+
+    /**
+     * Send message as packet to client
+     * @param message String of information to send to the client
+     */
+    private void sendData(String message){
+        try {
+            output.writeObject("Server>>" + message);
+            output.flush();
+        } catch (IOException ioException){
+            System.out.println("Error writing object.");
+        }
+    }
+
+    /**
+     * Closes all input/output streams and socket connections
+     * (probably not necessary but added for now just in case)
+     */
+    private void closeConnections(){
+        System.out.println("Terminating connection");
+        try{
+            output.close();
+            input.close();
+            connection.close();
+        } catch (IOException ioException){
             ioException.printStackTrace();
         }
     }
--- Ecommerce_Application/src/ServerTest.java
+++ Ecommerce_Application/src/ServerTest.java
@@ -2,5 +2,7 @@
  * Tests the Server class
  */
 public class ServerTest {
-    Server middleMan = new Server(); // needs better name, testing purposes for now
+    public static void main(String[] args){
+        Server middleMan = new Server(); // needs better name, testing purposes for now
+    }
 }
